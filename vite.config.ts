3f6118f01cd6419ded8d6a08fd8d4de61b43a792
Petru <podutz1234@gmail.com>
2025-05-25 10:00:13 +0300

    Fix CRITICAL: Schimbat host din '::' in '0.0.0.0' pentru a evita eroarea path-to-regexp

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
     allowedHosts: ["instalatiisanitarepro.ro"],
   },
