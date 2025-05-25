6ac4edaae734980f3a20399292b7bbf99a623d06
Petru <podutz1234@gmail.com>
2025-05-25 09:52:42 +0300

    Adaugare server Express pentru servire si cache, actualizare package.json

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'publish', 'index.html'));
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
